163d8d93c850b1a146ecc72439884f10e7319048
faisalfeahd@gmail.com <faisalfehad@gmail.com>
2016-06-02 13:54:35 +0000

    Create a failure test for creating posts

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper.rb'
+
+feature 'Creating Posts' do
+  scenario 'Can create post' do
+    visit '/'
+    click_link 'Create Post'
+    fill_in 'Title', with: 'title'
+    fill_in 'Content', with: 'post content'
+    click_button 'Create'
+    expect(page).to have_content 'title'
+    expect(page).to have_content 'post content'
+  end
+end
