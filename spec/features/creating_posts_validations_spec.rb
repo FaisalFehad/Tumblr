43e10fc171e744be4c8d6759c2f5fd6c5abdbcdb
FaisalFehad <faisalfehad@gmail.com>
2016-06-04 13:35:36 +0000

    Rename and fix validations spec

--- spec/features/creating_posts_validations_spec.rb
+++ spec/features/creating_posts_validations_spec.rb
@@ -8,15 +8,15 @@ feature 'Save valid posts only' do
     fill_in 'Title', with: options[:title]
     fill_in 'Content', with: options[:content]
     click_button "Create Post"
-    visit '/posts'
+    visit '/posts/'
   end
 
 char = 'a'
 
   it "Doesn't save posts with more than 21 characters " do
-    attempt_to_post_with title: (char * 22)
-    attempt_to_post_with content: (char * 15)
-    expect(page).not_to have_content (char * 22)
+    attempt_to_post_with title: (char *22)
+    attempt_to_post_with content: (char *15)
+    expect(page).not_to have_content (char *22)
   end
 
   it "Does save posts with vaild title and content" do
@@ -26,9 +26,12 @@ char = 'a'
     expect(page).to have_content (char *8)
     end
 
-  it "Doesn't save posts with 2 characters content" do
-    attempt_to_post_with title: (char *9)
-    attempt_to_post_with content: (char * 2)
-    expect(page).not_to have_content ("#{char}" *9)
-  end
+   it "Doesn't save posts with invalid content even with valid title" do
+     visit '/posts/new'
+     fill_in 'Title', with: (char *4)
+     fill_in 'Content', with: "a"
+     click_button "Create Post"
+     visit '/posts/'
+     page.should_not have_content (char *4)
+    end
 end
