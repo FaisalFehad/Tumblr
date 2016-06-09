4953adc7115939089e6618ddde5367798d78bf71
faisalfeahd@gmail.com <faisalfehad@gmail.com>
2016-06-09 19:21:01 +0000

    Missing colon/fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates :name, presence: true
-  validates :content, presence true, length {maximum: 140}
+  validates :content, presence: true, length: {maximum: 140}
 end
